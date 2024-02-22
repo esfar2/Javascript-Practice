4ef0f3849d402fb65a8079040f0088335e6a81b2
esfar2 <esfarsami2@gmail.com>
2024-02-22 16:20:33 +0600

    learning ES6

---
+++ MD-30/app.js
@@ -0,0 +1,18 @@
+// use let if you want to reassign the variable 
+// use const for most static reasons
+// use default parameters in function so that we don't get undefined
+// Usually for default parameters use empty strings,objects, arrays and numbers of your own choice
+// Backticks used for string interpolation and multi line strings
+// Spread operator usually used in objects and arrays
+// When spread operator is used in an array to copy the initial array then, it will not copy the reference to the           original array, instead it will create a new array
+
+const numbers = [10,12,14,15,16,17,18,19,20,21,22,23]
+const student = {
+  name: "Osama",
+  age: 27,
+  
+}
+
+
+
+
--- programming hero/app.js
+++ programming hero/app.js
@@ -1,5 +1,15 @@
-const liCollection = document.getElementsByTagName('li');
+// const liCollection = document.getElementsByTagName('li');
 // console.log(liCollection);
-for (const li of liCollection) {
-  // console.log(li.innerHTML);
-}
+// for (const li of liCollection) {
+//   console.log(li.innerHTML);
+// }
+// const fruits = document.getElementById('fruits-title');
+// console.log(fruits.innerText);
+// const places = document.getElementById('places-title');
+// const placeCollection = document.getElementsByClassName('place')
+// for(const place of placeCollection) {
+//   place.style.color = 'purple'
+// }
+
+const places = document.querySelector('.places-container  li');
+console.log(places);
