fb86a527738cab15c4f290e4f1e8d2998feae75b
esfar2 <esfarsami2@gmail.com>
2024-03-19 08:59:14 +0600

    done

---
+++ js-concepts-for-react/app.js
@@ -0,0 +1,14 @@
+/**
+ * How to declare variables let and const
+ * if conditions
+ * array declarations push,pop,shift,unshift,length
+ * loops - for, for of, for in
+ * function declarations
+ * Objects - access properties via . , ["string" / variable]
+ * ES6 Fundamentals
+ * Template Literals ``
+ * Arrow Functions =>
+ * Spread Operator ... => to copy exactly the array
+ * Array Methods map,forEach,filter,find
+ * Destructuring for arrays and objects
+ */
